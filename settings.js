8ec9a873a3446aa82c3f1cfd9bc76c91a74994ae
Enanferdo <83647972+Enanferdo@users.noreply.github.com>
2022-10-09 12:30:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['917005882130']
+global.premium = ['917005882130']
+global.ownernomer = '917005882130'
+global.ownername = 'Enan F Wahlang'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
